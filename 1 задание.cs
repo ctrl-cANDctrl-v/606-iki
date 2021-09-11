66e95cf715dec45c5ec1742d50f595cec98eda45
ctrl-cANDctrl-v <85559984+ctrl-cANDctrl-v@users.noreply.github.com>
2021-09-11 13:07:03 +0300

    Add files via upload

---
+++ 1 задание.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace da
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\\test.txt";
+            string[] readText = File.ReadAllLines(path);
+            bool g = true;
+            int i, j = str.Length;
+            for (i = 0; i < j / 2; i++)
+            {
+                if (str[i] != str[j - (i + 1)])
+                {
+                    g = false;
+                    break;
+                }
+            }
+
+            if (g)
+                Console.WriteLine("Является палиндромом");
+            else
+                Console.WriteLine("Не является палиндромом");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2 задание.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace da
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String path = @"C:\Users\528-19\Desktop\test.txt";
+            string[] readText = File.ReadAllLines(path);
+            foreach (string s in readText)
+            {
+                Console.WriteLine(s);
+                bool g = true;
+                int i, j = s.Length;
+                for (i = 0; i < j / 2; i++)
+                {
+                    if (readText[i] != readText[j - (i + 1)])
+                    {
+                        g = false;
+                        break;
+                    }
+                }
+
+                if (g)
+                    Console.WriteLine("Является палиндромом");
+                else
+                    Console.WriteLine("Не является палиндромом");
+                //Console.ReadLine();
+            }
+        }
+    }
+}
