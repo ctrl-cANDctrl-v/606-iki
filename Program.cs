21836b7d1e1f0d239f4c9eadd68f92a6448cd1b5
ctrl-cANDctrl-v <85559984+ctrl-cANDctrl-v@users.noreply.github.com>
2021-09-11 13:10:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace da
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  Console.WriteLine("Напишите слово или число");
+            string str = Console.ReadLine();
+            bool g = true;
+            int i, j = str.Length;
+            for (i = 0; i < j / 2; i++)
+             {
+                if (str[i] != str[j - ( i + 1)])
+                {
+                    g = false;
+                    break;
+                }
+             }
+
+            if (g)
+                Console.WriteLine("Является палиндромом");
+            else
+                Console.WriteLine("Не является палиндромом");
+           Console.ReadLine();
+        }
+    }
+}
